e577284d193b38761b2869b5ecf8bd148d49a570
Robert Danitz <robert.danitz@posteo.de>
2015-01-15 20:39:18 +0100

    initial commit
    Former-commit-id: 79b4db4bd74077ed5d45e5cb1b489081c3edfca5

---
+++ app/scripts/directives/frame.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name app.directive:frame
+ * @description
+ * # frame
+ */
+angular.module('app')
+  .directive('frame', function () {
+    return {
+      restrict: 'A',
+      template: '<div class="pure-u-1-3">foo<div class="frame"></div><div>',
+      link: function postLink(scope, element, attrs) {
+        attrs.align = 'center';
+      }
+    };
+  });
